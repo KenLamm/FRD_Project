abf0a10c1d0020c374171dd12747ee2d70856126
sam <chakkwanlee@gmail.com>
2023-06-07 16:19:09 +0800

    workingfolder

--- app/src/pages/workingfolderPages/workingFolder.tsx
+++ app/src/pages/workingfolderPages/workingFolder.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FaFolder, FaPlus } from 'react-icons/fa';
-import './workingFolder.css';
+import useStyles from './/workingFolderCss';
 import { Link } from 'react-router-dom';
 
 interface Folder {
@@ -10,18 +10,19 @@ interface Folder {
 }
 
 const FolderPage: React.FC = () => {
+  const { classes } = useStyles();
   const [folders, setFolders] = useState<Folder[]>([
-    { id: 1, name: 'Folder 1', path: '/path/to/folder1' },
-    { id: 2, name: 'Folder 2', path: '/path/to/folder2' },
-    { id: 3, name: 'Folder 3', path: '/path/to/folder3' },
-    { id: 4, name: 'Folder 4', path: '/path/to/folder4' },
+    { id: 1, name: '岩石地基', path: '/path/to/folder1' },
+    { id: 2, name: '降水工作', path: '/path/to/folder2' },
+    { id: 3, name: '石縫走向', path: '/path/to/folder3' },
+    { id: 4, name: '基石縫裂', path: '/path/to/folder4' },
   ]);
   const [newFolderName, setNewFolderName] = useState('');
   const [isAddingFolder, setIsAddingFolder] = useState(false);
 
-  const handleFolderClick = (path: string) => {
-    // Perform any actions when a folder is clicked, such as navigating to the folder path
-  };
+  // const handleFolderClick = (path: string) => {
+  //   // Perform any actions when a folder is clicked, such as navigating to the folder path
+  // };
 
   const handleAddClick = () => {
     if (newFolderName.trim() !== '') {
@@ -38,37 +39,37 @@ const FolderPage: React.FC = () => {
   };
 
   return (
-    <div className="container">
-      <h1 className="title">Working Folder Page</h1>
+    <div className={classes.container}>
+      <h1 className={classes.title}>地基檢測</h1>
       {folders.map(folder => (
         <Link
           key={folder.id}
           to="/photodetail"
-          className="folder-button"
+          className={classes.folderButton}
           // onClick={e => {
           //   e.preventDefault();
           //   handleFolderClick(folder.path);
           // }}
         >
-          <FaFolder className="folder-icon" /> {folder.name}
+          <FaFolder className={classes.folderIcon} /> {folder.name}
         </Link>
       ))}
       {isAddingFolder ? (
-        <div className="add-button-container">
+        <div className={classes.addButtonContainer}>
           <input
             type="text"
-            className="add-input"
+            className={classes.addInput}
             placeholder="Enter folder name"
             value={newFolderName}
             onChange={e => setNewFolderName(e.target.value)}
           />
-          <button className="add-button" onClick={handleAddClick}>
-            <FaPlus className="add-icon" />
+          <button className={classes.addButton} onClick={handleAddClick}>
+            <FaPlus className={classes.addIcon} />
           </button>
         </div>
       ) : (
-        <button className="add-button" onClick={() => setIsAddingFolder(true)}>
-          <FaPlus className="add-icon" /> Add
+        <button className={classes.addButton} onClick={() => setIsAddingFolder(true)}>
+          <FaPlus className={classes.addIcon} /> 
         </button>
       )}
     </div>
---
+++ app/src/pages/workingfolderPages/workingFolderCss.tsx
@@ -0,0 +1,76 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+
+  container: {
+    display: 'flex',
+    textAlign: 'center',
+    flexDirection: 'column',
+    padding: '40px',
+    borderRadius: '20px',
+    backgroundColor: "rgb(0, 0, 0)",
+    color:"rgb(255, 255, 255)",
+  },
+  workingFolderPage: {
+      fontSize: '24px',
+      marginBottom: '20px',
+  },
+  folderList: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+    gridGap: '20px',
+    maxWidth: '800px',
+    marginBottom: '20px',
+  },
+  folderButton: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '5px',
+    border: `3px solid rgb(0, 0, 0) `,
+    borderRadius: '15px',
+    textDecoration: 'none',
+    color: "rgb(0, 0, 0)",
+    backgroundColor:  "rgb(255, 187, 73)",
+    transition: 'background-color 0.3s ease',
+  },
+  folderIcon: {
+    fontSize: '29px',
+    marginBottom: '5px',
+    color: "rgb(255, 255, 255)",
+  },
+  addButton: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '10px',
+    border: 'none',
+    borderRadius: '5px',
+    backgroundColor: "rgb(0, 0, 0)",
+    color: "rgb(255, 187, 73)",
+    fontSize: '24px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s ease',
+    marginTop: '20px',
+    
+  },
+  addIcon: {
+    marginRight: '5px',
+    borderRadius: '5px',
+    backgroundColor: "rgm (0, 0, 0)",
+    color: "rgb(255, 187, 73)",
+  },
+  addInput: {
+    marginRight: '5px',
+  },
+  addButtonContainer:{
+    backgroundColor: "rgb(255, 187, 73)",
+  },
+  title:{
+    backgroundColor: "rgb(0, 0, 0)",
+    alignItems: 'center',
+  }
+
+}));
+
+export default useStyles
