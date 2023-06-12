13d045bc433b25f11986108314f90ca3224632ee
mavistangtpl <mavistangtpl@gmail.com>
2023-06-12 15:00:21 +0800

    update sql table

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PrismaModule, AuthModule],
   controllers: [],
   providers: [],
 })
