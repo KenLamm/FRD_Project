11112a77f00883a0cc27cd4e0b7981d3b4d59992
sam <chakkwanlee@gmail.com>
2023-06-12 11:35:52 +0800

    okok

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -5,6 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+
+  // method
   @Get()
   getHello(): string {
     return this.appService.getHello();
