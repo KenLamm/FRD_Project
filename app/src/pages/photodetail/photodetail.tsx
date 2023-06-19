d64833a025656faa0d12a89c39e122b35e21620c
林航 <kenlam@KenLam-Mac-Book-Pro.local>
2023-06-19 19:42:02 +0800

    css for photodetail

--- app/src/pages/photodetail/photodetail.tsx
+++ app/src/pages/photodetail/photodetail.tsx
@@ -45,7 +45,7 @@ const Photo: React.FC = () => {
 
   return (
     <div className="hi234" style={{ position: "relative" }}>
-      <h1 style={{ alignItems: "center" }}>{recordName&&recordName[0].name}</h1>
+      <h1 style={{ textAlign: "center" , color:"white", marginTop:0, paddingTop:'0.67em'}}>{recordName&&recordName[0].name}</h1>
       <div>
         <button className={classes.captureButton} onClick={handleCapturePhoto}>
           <FaCamera />
